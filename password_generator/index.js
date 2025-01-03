6cc9dfcf106144973b5dbe7b1c341f6735b5bb04
Agenorwoth Lleyton Adrian <lagenorwoth@gmail.com>
2025-01-04 01:46:39 +0300

    Add files via upload

---
+++ password_generator/index.js
@@ -0,0 +1,35 @@
+
+function generatePassword(length,includeLowerCase,includeNumbers,includeSymbols,includeUpperCase) {
+    
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "~!@#$%^&*()_+}{|><></>";
+
+    let allowedChars = "";
+    let password = "";
+    allowedChars += includeLowerCase ? lowercaseChars : "";
+    allowedChars += includeUpperCase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+        
+    }
+
+    return password;
+
+}
+
+const passwordLength = 10;
+const includeLowerCase = true;
+const includeUpperCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+const password = generatePassword(passwordLength,includeLowerCase,includeNumbers,includeSymbols,includeUpperCase);
+
+console.log(`Generated password: ${password}`);
